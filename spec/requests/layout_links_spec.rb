df96bf5960982e76c2282c20189dcc4d1b2447d6
10FirstGames <10FirstGames@gmail.com>
2012-03-11 13:21:01 -0500

    added links with routes

---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+
+	it "Should have a home page at '/'" do
+		get '/'
+		response.should have_selector('title', :content => "Home")
+	end
+
+	it "Should have a Contact page at '/contact'" do
+		get '/contact'
+		response.should have_selector('title', :content => "Contact")
+	end
+
+	it "Should have a about page at '/about'" do
+		get '/about'
+		response.should have_selector('title', :content => "About")
+	end
+
+	it "Should have a Help page at '/help'" do
+		get '/help'
+		response.should have_selector('title', :content => "Help")
+	end
+end
